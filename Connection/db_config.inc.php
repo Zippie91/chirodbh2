5c38b6ca8709ef16d2b6900bf7470e296ff2081b
Raf Smits <raf_smits@hotmail.com>
2014-11-10 13:47:00 +0100

    Add Gitignore and git attributes

--- Connection/db_config.inc.php
+++ Connection/db_config.inc.php
@@ -1,6 +1,6 @@
 <?php
-$servername = "mysql.serversfree.com";
-$user = "u164555197_chiro";
-$password = "rao2ygyc";
-$dbname = "u164555197_chiro";
+$servername = "localhost";
+$user = "root";
+$password = "";
+$dbname = "chirodon_chirodb";
 ?>
