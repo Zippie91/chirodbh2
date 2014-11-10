3cbacc2ff91f42f89c54bb3e8b285ab864622a54
Raf Smits <raf_smits@hotmail.com>
2014-11-10 13:21:06 +0100

    Build template.php by includes

---
+++ layout/_body-header.inc.php
@@ -0,0 +1,34 @@
+  <body>
+    <div id="wrapper">
+      <header>
+        <a href="#">
+          <img src="/ChiroDB2/images/logodbh.png" alt="Chiro Don Bosco Hoboken" />
+        </a>
+        <div id="header-slogan">
+          <img src="/ChiroDB2/images/T-shirt_achterkant_achter_elkaar_(kleur).jpg" alt="Slogan Chiro Don Bosco Hoboken" />
+        </div>
+      </header>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Afdelingen</a>
+            <ul class="child">
+              <li><a href="#">Ribbels</a></li>
+              <li><a href="#">Rakwi&acute;s</a></li>
+              <li><a href="#">Tito&acute;s</a></li>
+              <li><a href="#">KeA&acute;s</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Werkgroepen</a>
+            <ul class="child">
+              <li><a href="#">Leiding</a></li>
+              <li><a href="#">Veebees</a></li>
+              <li><a href="#">Ol&eacute;</a></li>
+              <li><a href="#">VZW</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Verhuur</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Links &amp; weetjes</a></li>
+        </ul>
+      </nav>
---
+++ layout/_head.inc.php
@@ -0,0 +1,5 @@
+<html>
+  <head>
+    <title>Chiro Don Bosco Hoboken</title>
+    <link rel="stylesheet" href="/ChiroDB2/CSS/main.css">
+  </head>
