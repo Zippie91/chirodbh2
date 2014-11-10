defb4d4cdad0a2539eeb11384792d467c842e1c1
Raf Smits <raf_smits@hotmail.com>
2014-11-10 14:05:45 +0100

    Add test.php and fix paths

--- test.php
+++ test.php
@@ -1,1 +1,33 @@
+<?php
+  include('layout/_head.inc.php');
+  include('layout/_body-header.inc.php');
 
+  /* Main content */
+  include('layout/_body-content-main.inc.php');
+  ?>
+  <!-- Body Main Content -->
+  <h1>Welkom</h1>
+  <p>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lacinia, velit vel viverra mollis, nulla massa ullamcorper ligula, sed tempor nisl enim et lorem. Pellentesque placerat blandit eros in bibendum. Ut posuere urna libero, vitae ultricies elit mollis at. Vivamus odio diam, egestas in facilisis a, hendrerit vel justo. Nunc mattis commodo ipsum, nec condimentum quam tempus id. Duis vitae rutrum sapien. Nunc varius placerat mauris sed convallis. Sed elit sem, lobortis vel ornare nec, consectetur id neque. Curabitur ullamcorper arcu sapien, a sollicitudin ipsum egestas et. Sed congue placerat turpis, quis auctor metus. Nunc vel egestas augue, eu viverra odio. Pellentesque pretium dui quam, ac fringilla massa iaculis non. Vestibulum tincidunt nisl ipsum, at volutpat mauris interdum eu. Donec maximus ultricies elit, vel pretium augue bibendum non. Fusce nec malesuada nibh. In hac habitasse platea dictumst.
+
+Sed rhoncus rhoncus risus, et lacinia odio venenatis id. Maecenas nec tristique metus. Quisque euismod sodales feugiat. Sed molestie neque eu sem gravida mattis. Nunc vestibulum auctor cursus. Phasellus suscipit lectus quis risus aliquet rutrum. Duis dapibus nibh lacus, eu egestas quam mattis lobortis. Sed eu risus sit amet turpis aliquam pharetra nec vitae velit. Aenean non mauris libero. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas placerat fringilla vulputate.
+
+Mauris faucibus enim at felis porttitor, nec varius nulla fermentum. Phasellus augue velit, bibendum ut sem sit amet, consequat eleifend arcu. Duis non volutpat augue. Sed mattis nibh finibus, molestie diam non, sagittis turpis. Nulla blandit interdum ultricies. Fusce sed dictum felis. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut sit amet neque quam. Curabitur blandit odio non eros pulvinar ultrices. Vivamus nec magna lobortis quam elementum rhoncus. Donec a ante augue. Aenean pretium lacus nec dui placerat, quis egestas sapien suscipit. In ac ligula in nisl placerat laoreet. Pellentesque malesuada euismod vulputate.
+
+Aenean lobortis justo a metus placerat mollis. Aliquam eu quam eros. Morbi ut ex lectus. Fusce pretium massa sem. Nam vitae fermentum velit. Aliquam auctor, nulla eget iaculis accumsan, orci ligula posuere lectus, a rutrum diam diam non lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam ligula metus, condimentum in vehicula ut, fringilla convallis arcu. Aenean ac dictum mauris. Sed nec mauris tempus sapien pellentesque mattis ac vitae diam. Curabitur non ex tellus. Etiam rhoncus pretium neque at luctus. Duis sollicitudin lobortis neque vel egestas. Donec luctus porta dignissim. Proin ac orci in ligula dignissim dictum at at mauris. Curabitur lobortis tortor lectus, consectetur ullamcorper nulla consequat quis.
+
+Sed mollis non urna volutpat euismod. Suspendisse posuere massa ante, sed blandit quam lobortis ut. Curabitur nisl libero, bibendum sit amet efficitur a, laoreet nec lorem. Aenean mollis congue neque quis consequat. Curabitur purus nisl, faucibus eget erat id, sodales lacinia mauris. Duis eget ex eu libero tempus ultrices. Vestibulum porttitor ornare nunc, venenatis molestie diam dictum eu. Nulla at tristique magna, id pretium turpis. Praesent tristique risus vel justo semper condimentum.
+  </p>
+  <!-- End Main Content -->
+  <?php
+  /* Aside */
+  include('layout/_body-content-aside.inc.php');
+  ?>
+  <!-- Body Aside -->
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia maximus lectus a luctus. Morbi elementum ipsum vitae tellus sagittis, et efficitur ante interdum. Nullam maximus lacinia ante sit amet luctus. Curabitur nec tortor rhoncus, bibendum odio quis, volutpat ex. Mauris vehicula lorem semper nulla congue, a porttitor ipsum ullamcorper. Quisque volutpat tortor ac sem imperdiet iaculis. Duis volutpat lacinia urna, eget aliquam mi interdum et. Ut vitae laoreet nibh.
+  </p>
+  <!-- End Aside -->
+  <?php
+  include('layout/_body-footer.inc.php');
+?>
