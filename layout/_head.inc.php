3759650ce733ba04c645c06af9f6f71246a1ea33
Raf Smits <raf_smits@hotmail.com>
2015-01-06 15:56:32 +0100

    add favicon

--- layout/_head.inc.php
+++ layout/_head.inc.php
@@ -3,4 +3,5 @@
     <title>Chiro Don Bosco Hoboken</title>
     <link rel="stylesheet" href="/chirodbh2/CSS/main.css">
     <link rel="stylesheet" href="/chirodbh2/CSS/gastenboek-aside.css">
+    <link rel="shortcut icon" href="/chirodbh2/images/chirologo_rounded_64.ico">
   </head>
